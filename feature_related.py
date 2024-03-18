3d3fcbc8300a1771917a2885f9575ae78a9678a8
henry880127 <henry880127@gmail.com>
2024-03-18 16:59:31 +0800

    Regression part added, so is model saving
    Regression part added, so is model saving

--- feature_related.py
+++ feature_related.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 class feature_extraction:
     def __init__(self, sampling_rate=500):
@@ -61,4 +62,17 @@ class feature_selection:
             si_c = si_c * (len(idx)/len(y)) # pi = len(idx)/len(y)
             sw += si_c
         score = sb / sw
+        return score
+    
+    def pearson_coef(self,x,y):
+        # compute pearson coefficient for each feature
+        # x: data, {numpy array}, shape (n_samples, n_features)
+        # y: numeric labels, {numpy array}, shape (n_samples, )
+        # return: {numpy array}, shape (n_features, )
+        n_samples, n_features = x.shape
+        score = np.zeros(n_features)
+        for i in range(n_features):
+            f = x[:,i]
+            score[i] = stats.pearsonr(f, y)[0]
+            score[i] = np.abs(score[i])
         return score
